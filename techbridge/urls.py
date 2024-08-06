5d025585a0fcb53388467a2aed5cfc235d62cc26
al23029 <al23029@shibaura-it.ac.jp>
2024-08-06 18:27:30 +0900

    Push

---
+++ techbridge/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', views.home, name='index')
+]
