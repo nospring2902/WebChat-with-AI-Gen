b349102d154f6945c566312218eed8049119c40b
Tran Ngoc Hung <kiaisoft.hung.tran@gmail.com>
2024-08-08 22:15:19 +0700

    Updating sign up

--- techbridge/models.py
+++ techbridge/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    main_language = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
--- techbridge/views.py
+++ techbridge/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 def prebase(request):
     return render(request, 'prebase.html')
@@ -7,5 +11,21 @@ def signin(request):
     return render(request, 'signin.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            print('Valid')
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')  # Chuyển hướng tới trang chủ hoặc trang dashboard sau khi đăng ký thành công
+        else:
+            print(form.errors)
+    else:  
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def logout(request):
+    return render(request, 'prebase.html')
